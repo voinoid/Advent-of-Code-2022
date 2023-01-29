fc722e806cdbb53f47ac30d755474b13a8147297
Mike Nye <MichaelTNye@gmail.com>
2023-01-29 12:05:55 +0200

    Updated Day 1

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,18 +1,22 @@
 if __name__ == '__main__':
-    NUM_CARRY = 3
-    f = open("input.txt", "r")
+    f = open("Day1/input.txt", "r")
     data = f.readlines()
     total_load = [0]
-    top_load = 0
+
     for item in data:
+        # clean data by removing new lines.
         num = item.rstrip()
+        # an empty line indicates a change in Elf and starts with NO load, else add elf's load.
         if num == "":
             total_load.append(0)
         else:
             total_load[-1] += int(num)
 
     total_load.sort()
-    top_load = sum(total_load[(-1*NUM_CARRY):])
-    #print(total_load)
+    #The Elf carrying the most
+    top_load = total_load[-1]
+    #The top 3 Elves carrying the most
+    top_three_loads = sum(total_load[-3:])
 
-    print(top_load)
+    print(f"Part 1) The heaviest load is: {top_load} caleries.")
+    print(f"Part 2) The 3 heaviest loads are: {top_three_loads} caleries.")
